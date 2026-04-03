a684c31ceab22c6873b4ad7c2442d256db5e5a56
nithya0312-n <rajaninn.1234@gmail.com>
2026-04-03 14:46:54 +0530

    Update file.py

--- file.py
+++ file.py
@@ -1,13 +1,4 @@
-# your_script.py
-
-def main():
-    print("Hello Jenkins, this is a simple Python script!")
-
-    # Write something to a log file so Jenkins can archive it
-    with open("pipeline_output.log", "w") as f:
-        f.write("Pipeline executed successfully.\n")
-        f.write("Message: Hello Jenkins!\n")
-
-if __name__ == "__main__":
-    main()
-
+num1 = 5
+num2 = 7
+print(num1 + num2) # Output: 12
+                                  
